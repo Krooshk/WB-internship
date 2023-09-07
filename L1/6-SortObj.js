eb4738e480e5c6d64355b905a7649618f1525e54
Krooshk <kirill.Krush@mail.ru>
2023-09-08 01:49:26 +0300

    add 6 task

---
+++ L1/6-SortObj.js
@@ -0,0 +1,33 @@
+// Задача о сортировке объектов: у вас есть массив объектов вида { name: 'John', age: 25 }.
+//  Напишите код, который сортирует этот массив по возрастанию возраста, 
+//  а при равных возрастах сортирует по алфавиту по полю name.
+
+console.log(sortObj([
+	{ name: 'Mike', age: 65 },
+	{ name: 'John', age: 55 },
+	{ name: 'Ann', age: 15 },
+	{ name: 'Vad', age: 30 },
+	{ name: 'Ivan', age: 30 },
+	{ name: 'Ann', age: 30 },
+	{ name: 'Alena', age: 30 },
+	{ name: 'Anton', age: 30 },
+	{ name: 'Boris', age: 30 },]))
+
+
+
+	function sortObj(arr){
+		return arr.sort((a,b) => {
+			if (a.age === b.age){
+				if (a.name === b.name){
+					return 0
+				} 
+				if (a.name > b.name) {
+					return 1
+				} else { return -1}
+			} else {
+				return a.age - b.age
+			}
+		})
+	}
+
+	
