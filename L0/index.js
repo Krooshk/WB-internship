3d115fde82c4b97a58a2cf228900e560d42c4025
Krooshk <kirill.Krush@mail.ru>
2023-09-09 18:45:42 +0300

    add popup payment-way

--- L0/index.js
+++ L0/index.js
@@ -8,3 +8,47 @@ accordionButtons.forEach((button) => {
 		button.querySelector('img').classList.toggle('rotate-180');
 	});
 });
+
+
+
+
+//--------- popup ---------// 
+
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+
+
+if (popupLinks.length > 0) {
+	for (let i = 0; i < popupLinks.length; i++) {
+		const popupLink = popupLinks[i];
+		popupLink.addEventListener("click", function (e) {
+			const popupName = popupLink.getAttribute('data-name');
+			const currentPopup = document.getElementById(popupName);
+			popupOpen(currentPopup);
+			e.preventDefault();
+		});
+	}
+}
+
+function popupOpen(currentPopup) {
+	if (currentPopup) {
+		const popupActive = document.querySelector('.popup.open');
+		if (popupActive) {
+			popupClose(popupActive, false);
+		}
+		let btnClose = currentPopup.querySelector('.btn-close-popup');
+		btnClose.addEventListener("click", (e) => popupClose(e.target.closest('.popup')));
+		body.classList.add('lock');
+		currentPopup.classList.add('open');
+		currentPopup.addEventListener("click", function (e) {
+			if (!e.target.closest('.popup__content')) {
+				popupClose(e.target.closest('.popup'));
+			}
+		});
+	}
+}
+
+function popupClose(popupActive) {
+	popupActive.classList.remove('open');
+	body.classList.remove('lock');
+}
