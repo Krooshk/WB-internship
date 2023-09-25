efa555eb282684073b322a4d05d90575ea2554b2
Krooshk <kirill.Krush@mail.ru>
2023-09-25 22:37:21 +0300

    realize main logic in guess game

---
+++ L3/guess/src/js/index.js
@@ -0,0 +1,83 @@
+let leftEdgeValue, rightEdgeValue, secretNumber;
+
+
+let range = document.querySelector('.range');
+let startBtn = document.querySelector('.start');
+let sentBtn = document.querySelector('.sent');
+let leftEdge = document.querySelector('.left-edge');
+let rightEdge = document.querySelector('.right-edge');
+let leftEdgeDisplay = document.querySelector('.current-left-edge');
+let rightEdgeDisplay = document.querySelector('.current-right-edge');
+let fieldForAnswers = document.querySelector('.field-for-answers');
+let count = document.querySelector('.count');
+let restart = document.querySelector('.restart');
+
+let countAttempt = 0;
+let parity = "";
+
+
+function startTheGame() {
+	if ((leftEdge.value >= 1) && (rightEdge.value <= 1000) && (leftEdge.value < rightEdge.value)) {
+		leftEdgeValue = Number(leftEdge.value);
+		rightEdgeValue = Number(rightEdge.value);
+		leftEdgeDisplay.textContent = leftEdgeValue;
+		rightEdgeDisplay.textContent = rightEdgeValue;
+		range.classList.add('unactive');
+		secretNumber = Math.floor(Math.random() * (rightEdgeValue - leftEdgeValue + 1)) + leftEdgeValue;
+		parity = (secretNumber % 2) === 0 ? "четное" : "нечетное";
+
+	} else {
+		alert('Проверьте значения');
+	}
+}
+
+function checkAnswer() {
+	countAttempt++;
+	count.textContent = countAttempt;
+	checkEveryThirdAttempt();
+	let num = fieldForAnswers.value;
+	if((num < leftEdgeValue) || (num > rightEdgeValue)){
+		console.log('Число меньше');
+		return;
+	}
+	if (num > secretNumber) {
+		console.log('Число меньше');
+		return;
+	}
+	if (num < secretNumber) {
+		console.log('Число больше');
+		return;
+	}
+
+	console.log('Вы угадали');
+}
+
+function checkEveryThirdAttempt() {
+	if ((countAttempt > 0) && ((countAttempt % 3) === 0)) {
+		console.log(`Число ${parity}`);
+	}
+}
+
+function restartFn() {
+	range.classList.remove('unactive');
+	countAttempt = 0;
+	leftEdgeValue = null;
+	rightEdgeValue = null;
+	secretNumber = null;
+	parity = "";
+	let inputs = [...document.querySelectorAll('input')];
+	inputs.forEach(input => input.value = "");
+	count.textContent = 0;
+	leftEdgeDisplay.textContent = "";
+	rightEdgeDisplay.textContent = "";
+
+
+}
+
+startBtn.addEventListener('click', startTheGame);
+sentBtn.addEventListener('click', checkAnswer);
+restart.addEventListener('click', restartFn);
+
+
+// Начать игру заново
+// 
