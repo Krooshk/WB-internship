cc0a99ddadf96bb0f5071cb390eda9df7647754d
Krooshk <kirill.Krush@mail.ru>
2023-09-26 11:47:33 +0300

    add style, add ability to play with comp-random version

--- L3/tic-tac/src/js/index.js
+++ L3/tic-tac/src/js/index.js
@@ -3,6 +3,7 @@ let popupRef = document.querySelector(".popup");
 let newgameBtn = document.getElementById("new-game");
 let restartBtn = document.getElementById("restart");
 let msgRef = document.getElementById("message");
+let wrapper = document.querySelector(".wrapper");
 
 //Winning Pattern Array
 let winningPattern = [
@@ -17,83 +18,83 @@ let winningPattern = [
 ];
 
 
-function stepComputer() {
-	let btns = document.querySelectorAll('.button-option[disabled = false]');
-	console.log(btns);
-}
 
-//Player 'X' plays first
+
+
 let xTurn = true;
 let count = 0;
-//Disable All Buttons
+
+
 const disableButtons = () => {
 	btnRef.forEach((element) => (element.disabled = true));
-	//enable popup
 	popupRef.classList.remove("hide");
 };
-//Enable all buttons (For New Game and Restart)
+
 const enableButtons = () => {
 	btnRef.forEach((element) => {
 		element.innerText = "";
 		element.disabled = false;
 	});
-	//disable popup
 	popupRef.classList.add("hide");
 };
-//This function is executed when a player wins
+
 const winFunction = (letter) => {
 	disableButtons();
 	if (letter == "X") {
-		msgRef.innerHTML = "&#x1F389; <br> 'X' Wins";
+		msgRef.innerHTML = "'X' Победил";
 	} else {
-		msgRef.innerHTML = "&#x1F389; <br> 'O' Wins";
+		msgRef.innerHTML = "'O' Победил";
 	}
 };
-//Function for draw
+
 const drawFunction = () => {
 	disableButtons();
-	msgRef.innerHTML = "&#x1F60E; <br> It's a Draw";
+	msgRef.innerHTML = "Ничья";
 };
-//New Game
+
 newgameBtn.addEventListener("click", () => {
 	count = 0;
 	enableButtons();
+	xTurn = true;
 });
+
 restartBtn.addEventListener("click", () => {
 	count = 0;
+	xTurn = true;
 	enableButtons();
 });
-//Win Logic
+
 const winChecker = () => {
-	//Loop through all win patterns
+
 	for (let i of winningPattern) {
 		let [element1, element2, element3] = [
 			btnRef[i[0]].innerText,
 			btnRef[i[1]].innerText,
 			btnRef[i[2]].innerText,
 		];
-		//Check if elements are filled
-		//If 3 empty elements are same and would give win as would
+
 		if (element1 != "" && (element2 != "") & (element3 != "")) {
 			if (element1 == element2 && element2 == element3) {
-				//If all 3 buttons have same values then pass the value to winFunction
 				winFunction(element1);
 			}
 		}
 	}
 };
-//Display X/O on click
+
+
 btnRef.forEach((element) => {
 	element.addEventListener("click", () => {
 		if (xTurn) {
 			xTurn = false;
 			//Display X
 			element.innerText = "X";
+			element.style.color = "#BD5532";
 			element.disabled = true;
 		} else {
 			xTurn = true;
 			//Display Y
 			element.innerText = "O";
+			element.style.color = "#2b243c";
 			element.disabled = true;
 		}
 		//Increment count on each click
@@ -102,9 +103,54 @@ btnRef.forEach((element) => {
 			drawFunction();
 		}
 		//Check for win on every click
-		stepComputer();
+		setTimeout(stepComputer,500);
+
 		winChecker();
 	});
 });
 //Enable Buttons and disable popup on page load
 window.onload = enableButtons;
+
+let btnGameWithFriend = document.querySelector('.btn-game-with-friend');
+let btnGameWithComp = document.querySelector('.btn-game-with-comp');
+let startPage = document.querySelector('.start-page');
+
+btnGameWithFriend.addEventListener("click", () => {
+	startPage.style.display = "none";
+	wrapper.style.display = "block";
+})
+
+btnGameWithComp.addEventListener("click", () => {
+	startPage.style.display = "none";
+	wrapper.style.display = "block";
+})
+
+
+let home = document.querySelector('.home');
+
+home.addEventListener("click", () => {
+	startPage.style.display = "flex";
+	wrapper.style.display = "none";
+})
+
+
+function stepComputer() {
+	let btns = [...document.querySelectorAll('.button-option')];
+	btns = btns.filter((el) => el.disabled === false);
+	let randomNum = Math.floor(Math.random() * btns.length);
+	let element = btns[randomNum];
+
+	xTurn = true;
+	element.innerText = "O";
+	element.style.color = "#2b243c";
+	element.disabled = true;
+
+	count += 1;
+	if (count == 9) {
+		drawFunction();
+	}
+	winChecker();
+}
+
+
+
