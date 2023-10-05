a2bd850b1dd2b3295b73b6de1e9a0c094732e029
Krooshk <98127179+Krooshk@users.noreply.github.com>
2023-10-05 20:06:17 +0300

    Update index.js

--- L1/24-table-data/index.js
+++ L1/24-table-data/index.js
@@ -25,7 +25,7 @@ const arrayOfRows = [...tbody.children];
 // В случае успеха преобразуем файлы в JSOn, затем присваиваем их data, вычисляем число страниц
 // Затем вызываем функцию fill, котоаря наполняет таблицу
 
-let dataFromFilltext = fetch("https://www.filltext.com/?rows=1000&fname=%7BfirstName%7D&lname=%7BlastName%7D&tel=%7Bphone%7Cformat%7D&address=%7BstreetAddress%7D&city=%7Bcity%7D&state=%7BusState%7Cabbr%7D&zip=%7Bzip%7D&pretty=true");
+let dataFromFilltext = fetch("http://www.filltext.com/?rows=1000&fname=%7BfirstName%7D&lname=%7BlastName%7D&tel=%7Bphone%7Cformat%7D&address=%7BstreetAddress%7D&city=%7Bcity%7D&state=%7BusState%7Cabbr%7D&zip=%7Bzip%7D&pretty=true");
 dataFromFilltext.then(value => value.json(), () => alert('Ошибка запроса'))
 	.then(value => {
 		data = value;
